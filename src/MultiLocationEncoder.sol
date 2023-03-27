455258c142bddd0c1be799a38819ffcd63645e43
lana-shanghai <svetlana.konstantinovna@gmail.com>
2023-03-27 15:58:01 +0200

    add parachain MultiLocation

--- src/MultiLocationEncoder.sol
+++ src/MultiLocationEncoder.sol
@@ -1,3 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-contract MultiLocationEncoder {}
+import "./XCMEnums.sol";
+import "./utils/ScaleCodec.sol";
+import "./utils/CompactTypes.sol";
+
+contract MultiLocationEncoder {
+
+    function encodeMultiLocationSameConsensusParachain(
+        uint8 parents,
+        uint256 parachainId
+    )
+    public pure returns (bytes memory) {
+        XCMEnums.XcmV3Junctions interiorJunctions = XCMEnums.XcmV3Junctions.X1;
+        XCMEnums.XcmV3Junction firstJunction = XCMEnums.XcmV3Junction.Parachain;
+        return abi.encodePacked(
+            ScaleCodec.encodeU8(uint8(parents)),
+            ScaleCodec.encodeU8(uint8(interiorJunctions)),
+            ScaleCodec.encodeU8(uint8(firstJunction)),
+            CompactTypes.encodeCompactUint(parachainId)
+        );
+    }
+
+    function encodeMultiLocationSameConsensusAccountId32(
+        uint8 parents,
+        uint256 parachainId,
+        bytes32 accountId32
+    )
+    public pure returns (bytes memory) {
+        XCMEnums.XcmV3Junctions interiorJunctions = XCMEnums.XcmV3Junctions.X2;
+        XCMEnums.XcmV3Junction firstJunction = XCMEnums.XcmV3Junction.Parachain;
+        XCMEnums.XcmV3Junction secondJunction = XCMEnums.XcmV3Junction.AccountId32;
+        return abi.encodePacked(
+            ScaleCodec.encodeU8(uint8(parents)),
+            ScaleCodec.encodeU8(uint8(interiorJunctions)),
+            ScaleCodec.encodeU8(uint8(firstJunction)),
+            CompactTypes.encodeCompactUint(parachainId),
+            ScaleCodec.encodeU8(uint8(secondJunction)),
+            accountId32
+        );
+    }
+}
--- test/MultiLocationEncoder.t.sol
+++ test/MultiLocationEncoder.t.sol
@@ -3,11 +3,24 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/MultiLocationEncoder.sol";
+import "../src/utils/CompactTypes.sol";
 
 contract MultiLocationEncoderTest is Test {
     MultiLocationEncoder public encoder;
 
     function setUp() public {
         encoder = new MultiLocationEncoder();
+    }
+
+    function testEncodeMultiLocationSameConsensusParachain() public {
+        bytes memory testValue = "\x00\x01\x00\xa1\x0f";
+        bytes memory encodedValue = encoder.encodeMultiLocationSameConsensusParachain(0, 1000);
+        assertEq(testValue, encodedValue);
+    }
+
+    function testEncodeMultiLocationSameConsensusAccountId32() public {
+        bytes memory testValue = "\x00\x02\x00\xa1\x0f\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+        bytes memory encodedValue = encoder.encodeMultiLocationSameConsensusAccountId32(0, 1000, 0x0000000000000000000000000000000000000000000000000000000000000000);
+        assertEq(testValue, encodedValue);
     }
 }
